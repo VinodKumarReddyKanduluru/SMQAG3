17476a914b22761f854d90494bc7e51d9d8538b4
Jayanth80 <152451551+Jayanth80@users.noreply.github.com>
2023-12-08 21:46:58 +0000

    Add files via upload

---
+++ src/main/java/com/example/model/Menu.java
@@ -0,0 +1,96 @@
+package com.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "menu_items")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name = "item_price")
+    private String itemPrice;
+
+    // Adjust the length of the item_id column
+    @Column(name = "item_id")
+    private String itemId;
+
+    @Column(name = "item_description")
+    private String itemDescription;
+
+    @Column(name = "veg_non_veg")
+    private String vegNonVeg;
+
+    @Column(name = "item_category")
+    private String itemCategory;
+
+    // Constructors, getters, and setters
+
+    public Menu() {
+        // Default constructor required by JPA
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getVegNonVeg() {
+        return vegNonVeg;
+    }
+
+    public void setVegNonVeg(String vegNonVeg) {
+        this.vegNonVeg = vegNonVeg;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+}
