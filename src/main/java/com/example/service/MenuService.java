966661a60d21db80ebfe413216b1ecaa4ed6bf3d
Jayanth80 <152451551+Jayanth80@users.noreply.github.com>
2023-12-08 21:52:32 +0000

    Add files via upload

---
+++ src/main/java/com/example/service/MenuService.java
@@ -0,0 +1,27 @@
+// MenuService.java
+package com.example.service;
+
+import com.example.model.Menu;
+import com.example.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    public List<Menu> getAllMenus() {
+        return menuRepository.findAll();
+    }
+
+    // Other service methods as needed...
+}
